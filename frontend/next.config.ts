220178a1dac12ac5bf7c7810a4c5ef1c3717f0ba
unknown <thenguyenfiner@gmail.com>
2025-02-20 19:23:22 +0700

    Initial push

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,125 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig: NextConfig = {
   experimental: {
     webVitalsAttribution: ['CLS', 'LCP', 'FCP', 'FID', 'TTFB'],
-  }
+    missingSuspenseWithCSRBailout: false,
+    turbo: {
+       resolveAlias: {
+         canvas: './empty-module.ts',
+       },
+     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.edgestore.dev'
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'e-cdns-images.dzcdn.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com'
+      },
+    ]
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }, 
+  trailingSlash: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  webpack(config, { isServer }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+      topLevelAwait: true,
+    };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+    if (!isServer) {
+      config.output.environment = { ...config.output.environment, asyncFunction: true };
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+      config.resolve.fallback.worker_threads = false;
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        ws: false,
+        dns: false,
+        net: false,
+        canvas: false,
+        encoding: false,
+        stream: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+  env: {
+    SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
+    SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET,
+    SHOPIFY_HOST: process.env.SHOPIFY_HOST,
+    SHOPIFY_API_URL: process.env.SHOPIFY_API_URL,
+    SHOPIFY_APP_NAME: process.env.SHOPIFY_APP_NAME,
+    AUTH_GOOGLE_CLIENT_SECRET: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    AUTH_GOOGLE_CLIENT_ID: process.env.AUTH_GOOGLE_CLIENT_ID,
+    GOOGLE_DEVELOPER_KEY: process.env.GOOGLE_DEVELOPER_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    APP_DOMAIN: process.env.APP_DOMAIN,
+    HOSTINGER_PASSWORD: process.env.HOSTINGER_PASSWORD,
+    NGROK_DOMAIN: process.env.NGROK_DOMAIN,
+    NGROK_URL: process.env.NGROK_URL,
+    EMAIL_TO: process.env.EMAIL_TO,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
+    UNSPLASH_SECRET_KEY: process.env.UNSPLASH_SECRET_KEY,
+    EDGE_STORE_ACCESS_KEY: process.env.EDGE_STORE_ACCESS_KEY,
+    EDGE_STORE_SECRET_KEY: process.env.EDGE_STORE_SECRET_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    PROXY_HOST: process.env.PROXY_HOST,
+    PROXY_PORT: process.env.PROXY_PORT,
+  },
 };
+
+module.exports = nextConfig;
+
 
 export default withSentryConfig(
   nextConfig,
--- frontend/src/app/blogs/layout.tsx
+++ frontend/src/app/blogs/layout.tsx
@@ -1,31 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import { Spinner } from "@/components/spinner";
-import { useStoreUser } from "@/hooks/use-store-user";
 
 const BlogLayout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
-  const { isAuthenticated, isLoading } = useStoreUser();
-
-  if (!isAuthenticated && !isLoading) return;
-
-  if (isLoading) {
-    return (
-      <div className="min-h-screen h-full flex items-center justify-center">
-        <Spinner size="lg" />
-      </div>
-    );
-  }
-
-  if (!isAuthenticated) {
-    redirect("/");
-  }
-
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
       {children}
